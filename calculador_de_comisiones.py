ce1b6dc2dd4f154e0574ee0f1328ebb3116fa215
GemmaOro <136896339+GemmaOro@users.noreply.github.com>
2023-06-19 15:07:42 +0200

    Delete calculador_de_comisiones.py

--- calculador_de_comisiones.py
+++
@@ -1,8 +0,0 @@
-nombre = input("Introduce tu nombre: ")
-ventas = input("Introduce el numero de ventas totales mensuales: ")
-
-ventas = int(ventas)
-resultado_comisiones = round(ventas*13/100,2)
-
-print(f"Enhorabuena {nombre} tus comisiones para este mes son {resultado_comisiones}")
-
